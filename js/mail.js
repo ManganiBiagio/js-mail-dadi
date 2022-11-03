c24758a297be88997178120a8b7881fb79c6e717
ManganiBiagio <biagiomangani@live.it>
2022-11-03 16:17:44 +0100

    html email

--- js/mail.js
+++ js/mail.js
@@ -1,17 +1,27 @@
-const emailInput=prompt("inserisci email");
-console.log(emailInput);
-const emails=["paperino@email.it","topolino@email.it","pippo@email.it","minni@email.it"];
-let validation=false;
 
-for(let i=0;i<emails.length;i++){
-    if(emails[i]===emailInput){
-        validation=true;
+
+const emailEl = document.querySelector("[name='email']");
+const btnEl = document.querySelector("#btn-accedi");
+
+btnEl.addEventListener("click", myFunction);
+
+
+function myFunction() {
+    const emails = ["paperino@email.it", "topolino@email.it", "pippo@email.it", "minni@email.it"];
+    let validation = false;
+    const emailInput = emailEl.value;
+
+
+    for (let i = 0; i < emails.length; i++) {
+        if (emails[i] === emailInput) {
+            validation = true;
+        }
     }
-}
 
-if(validation){
-    console.log("email valida")
-}
-else{
-    console.log("crea un account");
+    if (validation) {
+        console.log("email valida")
+    }
+    else {
+        console.log("crea un account");
+    }
 }
